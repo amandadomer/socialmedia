68c9ca5855a7689e9f8c964d9e298e06f284b5f4
RoamingKilroy <kilroy.zachary@gmail.com>
2021-10-27 19:08:04 -0400

    Installed React router and setup login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
+import Login from "./components/Login"
 
 function App() {
   return (
     <div className="App">
-      <h2>Test</h2>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
