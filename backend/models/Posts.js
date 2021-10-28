d6a48d6d0b5654b351e2db214626a67f963f0673
RoamingKilroy <kilroy.zachary@gmail.com>
2021-10-28 18:46:02 -0400

    Update Posts.js

--- backend/models/Posts.js
+++ backend/models/Posts.js
@@ -13,20 +13,11 @@ const PostSchema = new Schema({
   user: { type: Schema.Types.ObjectId, },
   text: { type: String, required: true, },
   name: { type: String,},
-<<<<<<< HEAD
-  likes: [{
-  user: { type: Schema.Types.ObjectId, },},],
-=======
   likes: [{user: { type: Schema.Types.ObjectId, }}],
   comments: {type:[commentSchema], default:[]},
->>>>>>> 2b13c0b393398e81f25433bfbd721e73d9e211f0
   date: { type: Date, default: Date.now, },
+});
 
-<<<<<<< HEAD
-  });
-
-module.exports = mongoose.model("post", PostSchema);
-=======
 const Comment = mongoose.model('comment', commentSchema);
 const Post = mongoose.model('post', PostSchema);
 
@@ -34,4 +25,3 @@ module.exports = {
   Comment,
   Post
 }
->>>>>>> 2b13c0b393398e81f25433bfbd721e73d9e211f0
